610960a78bab4284f73e76153d85a015f905033e
Alice Yu <ayu2013@gmail.com>
2017-06-30 14:01:35 -0500

    Most of the code

--- logic.js
+++ logic.js
@@ -2,24 +2,42 @@
  * Created by ayu20_000 on 6/30/2017.
  */
 
+$("document").ready(function(){
+    $("#submitSearch").click(function(){
+        event.preventDefault();
+        console.log($("#endYear").val());
+        getData($("#searchTerm").val(), $("#numberOfRecords").val(), $("#startYear").val(), $("#endYear").val());
+    })
+});
+
 function getData(userInput, numberOfRecords, startYear, endYear) {
+    let parameters={
+        'api-key': "20ee80a3bd064782998614ecf5e2941a"
+        , 'q': userInput
+        , 'page':""
+        , 'begin_date': startYear + "0101"
+        , 'end_date': endYear + "1231"
+    };
+    if(startYear === ''){
+        parameters['begin_date'] = "19900101";
+    }
+    if(endYear === ''){
+        parameters['end_date'] = "20170101";
+    }
+    //console.log(parameters);
     for (let i = 1; i <= numberOfRecords; i++) {
             let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+            parameters['page'] = i.toString();
+            console.log(parameters);
             url += '?'
-                + $.param({
-                    'api-key': "8550982747844a309bbc3751211f7458"
-                    , 'q': userInput
-                    , 'page': i.toString()
-                    , 'begin_date': startYear + "0101"
-                    , 'end_date': endYear + "1231"
-                });
+                + $.param(parameters);
 
             $.get(url).done(function (result) {
                 displayData(result.response.docs);
                 console.log(result);
             }).fail(function (result) {
                 setTimeout(function(){
-                    getData(userInput, numberOfRecords, startYear, endYear);
+                    getData(userInput, i, startYear, endYear);
                 }, 5000)
             });
     }
@@ -28,9 +46,10 @@ function getData(userInput, numberOfRecords, startYear, endYear) {
 function displayData(articleSet){
     for(let i = 0; i < articleSet.length; i++){
         let currentArticle = articleSet[i];
-        console.log(currentArticle.headline.main, currentArticle.byline.original, currentArticle.web_url, currentArticle.pub_date);
-
-        $(".articleDisplay").append(($("<div>").text(currentArticle.headline.main)), ($("<div>").text(currentArticle.byline.original)), ($("<div>").text(currentArticle.web_url)), ($("<div>").text(currentArticle.pub_date)));
+        let byline = currentArticle.byline["original"];
+        if(byline===null){
+            byline = "";
+        }
+        $(".articleDisplay").append(($("<div>").text(currentArticle.headline.main)), ($("<div>").text(currentArticle.byline["original"])), ($("<div>").text(currentArticle.web_url)), ($("<div>").text(currentArticle.pub_date)));
     }
 }
-getData("cake", 1, "2010", "2010");
